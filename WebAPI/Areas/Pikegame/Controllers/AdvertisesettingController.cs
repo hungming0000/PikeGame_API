2fd8d410dcc19b8b917ed2afbd35df49eefe22c0
hungming0000 <109592528+hungming0000@users.noreply.github.com>
2022-12-22 18:33:18 +0800

    1.新增廣告SQL
    1.新增廣告SQL

---
+++ WebAPI/Areas/Pikegame/Controllers/AdvertisesettingController.cs
@@ -0,0 +1,38 @@
+﻿using isRock.Framework.WebAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace WebAPI.Areas.Pikegame.Controllers
+{
+    public class AdvertisesettingController : ApiController
+    {
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        [Route("Pikegame/Advertisesetting/{MethodName}")]
+        [HttpPost]
+        public IHttpActionResult ExecuteMethod(string MethodName)
+        {
+            try
+            {
+                //AssemblyLauncher
+                AssemblyLauncher assemblyLauncher = new AssemblyLauncher();
+                //執行指定的Method                
+                var ret = assemblyLauncher.ExecuteCommand(
+                    new WebBO.Areas.Pikegame.Controllers.AdvertisesettingController(),
+                    MethodName,
+                   Request.Content.ReadAsStringAsync().Result
+                    );
+                //回傳OK
+                return Ok(ret);
+            }
+            catch (Exception ex)
+            {
+                //其他處理
+                throw ex;
+            }
+        }
+    }
+}
---
+++ WebBO/Areas/Pikegame/Controllers/AdvertisesettingController.cs
@@ -0,0 +1,52 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebBO.General;
+using WebBO.General.Repository.Connection;
+
+namespace WebBO.Areas.Pikegame.Controllers
+{
+   public  class AdvertisesettingController
+    {
+        protected ConnectionFactory _connectionFactory;
+        public AdvertisesettingController()
+        {
+            _connectionFactory = new ConnectionFactory();
+        }
+
+		/// <summary>
+		/// 取得比賽場次列表
+		/// </summary>
+		/// <returns></returns>
+		public ExecuteCommandAPIResult GetAdvertisesetting()
+		{
+			IDbConnection cn = _connectionFactory.CreateConnection("Pgsql");
+			string message = "";
+			bool isSuccess = true;
+
+			StringBuilder querySql = new StringBuilder();
+			querySql.Append(@"			         
+					SELECT *
+					FROM PUBLIC.advertisesetting
+					WHERE is_timeperiod_exist(advertiseid, periodtimes) = true
+            ");
+
+			var dt = new DataTable();
+			dt.Load(cn.ExecuteReader(querySql.ToString()));
+
+
+			return new ExecuteCommandAPIResult()
+			{
+				isSuccess = isSuccess,
+				Message = message,
+				Data = dt,
+				Count = dt.Rows.Count,
+			};
+		}
+
+	}
+}
