3e5a155c19312dca3c43e681d7dd4d53f1ca83ec
hungming0000 <109592528+hungming0000@users.noreply.github.com>
2022-12-23 19:51:48 +0800

    1.create ad
    1.create ad

--- WebBO/Areas/Pikegame/Controllers/AdvertisesettingController.cs
+++ WebBO/Areas/Pikegame/Controllers/AdvertisesettingController.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebBO.Areas.Pikegame.Models;
 using WebBO.General;
 using WebBO.General.Repository.Connection;
 
@@ -18,11 +19,12 @@ namespace WebBO.Areas.Pikegame.Controllers
             _connectionFactory = new ConnectionFactory();
         }
 
-		/// <summary>
-		/// 取得比賽場次列表
-		/// </summary>
-		/// <returns></returns>
-		public ExecuteCommandAPIResult GetAdvertisesetting()
+        #region 取得廣告輪播列表
+        /// <summary>
+        /// 取得廣告輪播列表
+        /// </summary>
+        /// <returns></returns>
+        public ExecuteCommandAPIResult GetAdvertisesetting()
 		{
 			IDbConnection cn = _connectionFactory.CreateConnection("Pgsql");
 			string message = "";
@@ -30,11 +32,21 @@ namespace WebBO.Areas.Pikegame.Controllers
 
 			StringBuilder querySql = new StringBuilder();
 			querySql.Append(@"			         
-					SELECT *
-					FROM PUBLIC.advertisesetting
-					WHERE is_timeperiod_exist(advertiseid, periodtimes) = true
-            ");
+					SELECT  advertiseid,
+									advertiseurl,
+									case adsstatus when 0 then '下架' when 1 then '上架' end adsstatus,
+									advertisstarttime::TEXT,
+									advertisendtime::TEXT,
+									advertistimeperiod,
+									advertiscosts,
+									modifydate::TEXT,
+									modifyuser,
+									periodtimes
+								FROM PUBLIC.advertisesetting
+								WHERE is_timeperiod_exist(advertiseid, periodtimes) = true
 
+            ");
+			
 			var dt = new DataTable();
 			dt.Load(cn.ExecuteReader(querySql.ToString()));
 
@@ -47,6 +59,77 @@ namespace WebBO.Areas.Pikegame.Controllers
 				Count = dt.Rows.Count,
 			};
 		}
+        #endregion
 
-	}
+
+        #region 裁判輸入分數
+
+        /// <summary>
+        /// 裁判輸入分數
+        /// </summary>
+        /// <returns></returns>
+        public ExecuteCommandAPIResult CreateAdvertise(AdvertisesettingModel request)
+        {
+            IDbConnection cn = _connectionFactory.CreateConnection("Pgsql");
+            string message = "";
+            bool isSuccess = true;
+            StringBuilder querySql = new StringBuilder();
+            var parm = new DynamicParameters();
+			string advertistimeperiod = request.advertistimeperiod;
+
+			string[] arrays = advertistimeperiod.Split(',');
+			request.periodtimes = arrays.Length;
+
+
+			#region  sql
+			querySql.Append(@"
+
+							INSERT INTO PUBLIC.advertisesetting (
+								advertiseurl,
+								adsstatus,
+								advertisstarttime,
+								advertisendtime,
+								advertistimeperiod,
+								advertiscosts,
+								modifydate,
+								modifyuser,
+								periodtimes
+								)
+							VALUES (
+								@advertiseurl,
+								@adsstatus,
+								@advertisstarttime,
+								@advertisendtime,
+								@advertistimeperiod,
+								@advertiscosts,
+								now(),
+								@modifyuser,
+								@periodtimes
+								)				
+			       ");
+            #endregion            
+            parm.Add("@advertiseurl", request.advertiseurl);
+            parm.Add("@adsstatus", Convert.ToInt32( request.adsstatus));
+            parm.Add("@advertisstarttime", request.advertisstarttime);
+            parm.Add("@advertisendtime", request.advertisendtime);
+            parm.Add("@advertistimeperiod", request.advertistimeperiod);
+            parm.Add("@advertiscosts", request.advertiscosts);
+            parm.Add("@modifyuser", request.modifyuser);
+            parm.Add("@periodtimes", request.periodtimes);
+
+            var dt = new DataTable();
+            dt.Load(cn.ExecuteReader(querySql.ToString(), parm));
+
+            return new ExecuteCommandAPIResult()
+            {
+                isSuccess = isSuccess,
+                Message = message,
+                Data = dt,
+                Count = dt.Rows.Count,
+            };
+        }
+
+        #endregion
+
+    }
 }
---
+++ WebBO/Areas/Pikegame/Models/AdvertisesettingModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebBO.Areas.Pikegame.Models
+{
+    public class AdvertisesettingModel
+    {
+        public int advertiseid { get; set; }
+        public string advertiseurl { get; set; }
+        public string  adsstatus { get; set; }
+        public string adsstatusName { get; set; }
+        public DateTime advertisstarttime { get; set; }
+        public DateTime advertisendtime { get; set; }
+        public string advertistimeperiod { get; set; }
+        public int advertiscosts { get; set; }
+        public DateTime modifydate { get; set; }
+        public string modifyuser { get; set; }
+        public int periodtimes { get; set; }
+    }
+}
